4f67e98c0dddbd54521fa0fec226701a2fb7e7b1
phil <pwhitby1981@gmail.com>
2020-05-11 15:06:48 +0100

    adding weathewr api key

--- assests/js/weather.js
+++ assests/js/weather.js
@@ -4,3 +4,7 @@ const tempElement = document.querySelector('.temperature-value p');
 const descElement = document.querySelector('.temperature-description p');
 const locationElement = document.querySelector('.location p');
 const notificationElement = document.querySelector('.notification');
+
+class Weather {
+  constructor(city) {
+    this.apiKey = 'b5c7508c8f7b5e6bad730dc858c0756a';
