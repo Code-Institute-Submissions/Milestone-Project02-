3d19d186551b3afac4318fc23c2f7dcb7f5ed4de
phil <pwhitby1981@gmail.com>
2020-05-12 06:47:33 +0100

    testing how to have nulitple maps via goolgemaps api

--- assests/js/maps.js
+++ assests/js/maps.js
@@ -1,19 +1,16 @@
 //conact page general map added:
 
-var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
     center: { lat: -16.5004, lng: -151.7415 },
-    zoom: 12,
   });
 }
 
 //Experinces maps added:
-
-var map2;
 function initMap() {
-  map2 = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: -16.5004, lng: -151.7415 },
+  var map1 = new google.maps.Map(document.getElementById('mapTwo'), {
     zoom: 15,
+    center: { lat: -16.5004, lng: -151.7415 },
   });
 }
