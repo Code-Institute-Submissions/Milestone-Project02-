416796b4a9b0f19214156a376cd0bc911c785708
phil <pwhitby1981@gmail.com>
2020-05-12 07:37:12 +0100

    fixed contact map, experinces maps casuing issues

--- assests/js/maps.js
+++ assests/js/maps.js
@@ -6,11 +6,3 @@ function initMap() {
     center: { lat: -16.5004, lng: -151.7415 },
   });
 }
-
-//Experinces maps added:
-function initMap() {
-  var map1 = new google.maps.Map(document.getElementById('mapTwo'), {
-    zoom: 15,
-    center: { lat: -16.5004, lng: -151.7415 },
-  });
-}
