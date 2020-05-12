dbeb38da10ee312ef14ada4dab459dd1de1655c1
phil <pwhitby1981@gmail.com>
2020-05-12 16:59:24 +0100

    trying to fix maps issue

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -6,3 +6,8 @@ function initMap() {
     center: { lat: -16.5004, lng: -151.7415 },
   });
 }
+
+var map1 = new google.maps.Map(document.getElementById('map1'), {
+  zoom: 15,
+  center: { lat: -16.5004, lng: -151.7415 },
+});
