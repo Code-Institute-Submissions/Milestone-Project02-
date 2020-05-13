06b74a214210d08ad8f90bfa01564e0de7ce4a31
phil <pwhitby1981@gmail.com>
2020-05-13 07:58:04 +0100

    fixed const missing on url as it wont be changing

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,25 +1,25 @@
 const cityName = 'Vaitape';
 const appID = 'b5c7508c8f7b5e6bad730dc858c0756a';
-http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${appID}.json
+const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${appID}.json`;
+
 function fetchWeatherData() {
   let req = $.ajax({
-      url: url,
-      type: 'GET',
-      error: errorHandle,
-      success: successHandle,
+    url: url,
+    type: 'GET',
+    error: errorHandle,
+    success: successHandle,
   });
-
 
   function errorHandle(obj, status, errorThrown) {
     console.log(status);
+  }
+
+  function successHandle(data, textStatus, jqXHR) {
+    console.log(data);
+    // dynamically build html based on the data
+  }
 }
 
-
-
-
-
-
-
-
-
-
+$(document).ready(function (event) {
+  fetchWeatherData();
+});
