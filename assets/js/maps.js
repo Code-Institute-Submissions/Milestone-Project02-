152ab1281abad7f48f7c42ac4921636ec592c711
phil <pwhitby1981@gmail.com>
2020-05-21 13:07:38 +0100

    added style to google maps, add 4 google maps, 3 to experiences and 1 to contact

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,10 +1,24 @@
-//Restaurant St.James, first map in Experinceces
+//Contact page
 
 //The Lagoonarium Third map in Experinceces
 
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 13,
-    center: { lat: -16.5004, lng: -151.7415 },
-  });
+var map1, map2, map3, map4;
+function drawMap() {
+  var mapOptions = {
+    zoom: 17,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    mapTypeControl: true,
+    fullscreenControl: false,
+  };
+  mapOptions.center = new google.maps.LatLng(-16.499977, -151.752342); // Restaurant St.James, first map in Experinceces
+  map1 = new google.maps.Map(document.getElementById('restaurant'), mapOptions);
+
+  mapOptions.center = new google.maps.LatLng(-16.475653, -151.772469); // Bora-Bora Pearl Beach Resort & Spa
+  map2 = new google.maps.Map(document.getElementById('spa'), mapOptions);
+
+  mapOptions.center = new google.maps.LatLng(-16.489811, -151.71422); // Bora Bora Lagoonarium
+  map3 = new google.maps.Map(document.getElementById('tours'), mapOptions);
+
+  mapOptions.center = new google.maps.LatLng(-16.497441, -151.73605); // Bora Bora
+  map4 = new google.maps.Map(document.getElementById('boraBora'), mapOptions);
 }
