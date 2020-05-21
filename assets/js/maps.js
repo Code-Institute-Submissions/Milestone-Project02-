06bca98dc2c302ef96d20803df318bb6a3f8985a
phil <pwhitby1981@gmail.com>
2020-05-21 10:46:47 +0100

    modified contact page, restarting elements of experinces page

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,17 +1,10 @@
 //Restaurant St.James, first map in Experinceces
-var map1, map2;
-function drawMap() {
-  var mapOptions = {
-    zoom: 16,
-    mapTypeId: google.maps.MapTypeId.PLACESMAP,
-    mapTypeControl: true,
-    fullscreenControl: false,
-  };
-  mapOptions.center = new google.maps.LatLng(-16.499977, -151.752342); // London
-  map1 = new google.maps.Map(document.getElementById('map1'), mapOptions);
-
-  mapOptions.center = new google.maps.LatLng(52.370216, 4.895168); // Amsterdam
-  map2 = new google.maps.Map(document.getElementById('spa'), mapOptions);
-}
 
 //The Lagoonarium Third map in Experinceces
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: { lat: -16.5004, lng: -151.7415 },
+  });
+}
