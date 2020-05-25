19b7b8fc73875ff7994de3e5dddd992c4587372f
phil <pwhitby1981@gmail.com>
2020-05-25 16:53:57 +0100

    removed double coded after trying to foix error

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -41,47 +41,10 @@ $(document).ready(function () {
     }
   });
 
-  var settings = {
-    async: true,
-    crossDomain: true,
-    url: 'baseURL',
-    method: 'GET',
-    dataType: 'json',
-    data: { lat: 'lat' },
-    headers: {
-      'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
-      'x-rapidapi-key': 'e0424dd9b4msh9d649b578e70578p1d5f61jsnf2890bff5c2f',
-    },
-    success: successFunction,
-    error: errorFunction,
-  };
-
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-  });
-
-  function successFunction(data, textStatus, jqXHR) {
-    let icon =
-      'https://openweathermap.org/img/wn/' + data.weather[0].icon + '.png';
-    let temp = Math.floor(data.main.temp);
-    let weather = data.weather[0].main;
-    let wind = data.wind.speed;
-
-    $('.city').text('City: ' + city);
-    $('.icon').attr('src', icon);
-    $('.weather').text('Weather: ' + weather);
-    $('.temp').text('Temperature: ' + temp);
-    $('.wind').text('Wind: ' + wind);
-  }
-
-  function errorFunction(response) {
-    console.log(response);
+  // Display Weather
+  function displayWeather() {
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
   }
 });
-
-// Display Weather
-function displayWeather() {
-  iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
-  tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-  descElement.innerHTML = weather.description;
-}
