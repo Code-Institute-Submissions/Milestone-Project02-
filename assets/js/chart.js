cfab36cfe4fb245147891bbba7e6ad5d6033dd83
phil <pwhitby1981@gmail.com>
2020-05-26 11:55:04 +0100

    changed chart format and updated testimonals in about page

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,41 +1,44 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true,
-          },
-        },
-      ],
+var tempCanvas = document.getElementById('tempChart');
+
+Chart.defaults.global.defaultFontFamily = 'lato';
+Chart.defaults.global.defaultFontSize = 15;
+
+var monthData = {
+  labels: [
+    'January',
+    'Febuary',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ],
+  datasets: [
+    {
+      label: 'Daytime Avg High Temperature in ºC',
+      data: [30, 31, 31, 31, 30, 29, 29, 28, 29, 29, 30, 30],
+    },
+  ],
+};
+
+var chartOptions = {
+  legend: {
+    display: true,
+    position: 'top',
+    labels: {
+      boxWidth: 50,
+      fontColor: 'black',
     },
   },
+};
+
+var lineChart = new Chart(tempCanvas, {
+  type: 'line',
+  data: monthData,
+  options: chartOptions,
 });
