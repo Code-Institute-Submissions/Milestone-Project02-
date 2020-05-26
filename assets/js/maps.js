de5ef0ff1cc9cd60215becdc8ab654a753222e03
phil <pwhitby1981@gmail.com>
2020-05-26 11:58:43 +0100

    made map links more clear what it is for

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -8,7 +8,7 @@ function drawMapContact() {
 
 //The Lagoonarium Third map in Experinceces
 
-var map1, map2, map3;
+var restaurant, spa, tours;
 function drawMapsExperiences() {
   var mapOptions = {
     zoom: 17,
@@ -17,11 +17,14 @@ function drawMapsExperiences() {
     fullscreenControl: false,
   };
   mapOptions.center = new google.maps.LatLng(-16.499977, -151.752342); // Restaurant St.James, first map in Experinceces
-  map1 = new google.maps.Map(document.getElementById('restaurant'), mapOptions);
+  restaurant = new google.maps.Map(
+    document.getElementById('restaurant'),
+    mapOptions
+  );
 
   mapOptions.center = new google.maps.LatLng(-16.475653, -151.772469); // Bora-Bora Pearl Beach Resort & Spa
-  map2 = new google.maps.Map(document.getElementById('spa'), mapOptions);
+  spa = new google.maps.Map(document.getElementById('spa'), mapOptions);
 
   mapOptions.center = new google.maps.LatLng(-16.489811, -151.71422); // Bora Bora Lagoonarium
-  map3 = new google.maps.Map(document.getElementById('tours'), mapOptions);
+  tours = new google.maps.Map(document.getElementById('tours'), mapOptions);
 }
